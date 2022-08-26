31e15dd3063facabf3d300060eb6267354ccef9a
LUCAS <LUCAS@177.8.110.86>
2022-08-25 21:45:31 -0300

    Teste de commit

--- TT001project/src/main/java/model/Main.java
+++ TT001project/src/main/java/model/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         Cliente c1 = new Cliente(1, "Lucas", "34473628809","13484141", "l247536@dac.unicamp.br");
+        Cliente c2 = new Cliente(2,"Bruno", "5481654651","653215122","bruno@gmail.com");
         System.out.println("Nome: "+c1.getNome());
+        System.out.println("Nome: "+c2.getNome());
     }
 }
